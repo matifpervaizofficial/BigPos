1ca6d419056c8bf3d4f0b1d12bd15e398905a407
Muhammad Atif Pervaiz <91389997+matifpervaizofficial@users.noreply.github.com>
2022-09-01 23:48:46 +0500

    Search Page

---
+++ datamodel.dart
@@ -0,0 +1,138 @@
+class UserData {
+  final int id;
+  final String name;
+
+  final String desc;
+  final String location;
+  final double groupprice;
+  final double personprice;
+  final double consultingprice;
+  final String userimage;
+  final String galleryimg1;
+  final String galleryimg2;
+  final String galleryimg3;
+  final String galleryimg4;
+
+  UserData(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.location,
+      required this.groupprice,
+      required this.personprice,
+      required this.consultingprice,
+      required this.userimage,
+      required this.galleryimg1,
+      required this.galleryimg2,
+      required this.galleryimg3,
+      required this.galleryimg4});
+}
+
+class UserInfo {
+  static final users = [
+    UserData(
+        id: 1,
+        name: "Selina kyle",
+        desc: "Trainer, Nutriotionist, consultant, Physiotherapist etc.",
+        location: "Fuerstenfeldbruck, Germa... ",
+        groupprice: 10,
+        personprice: 15,
+        consultingprice: 30,
+        userimage: "assets/images/user.jpg",
+        galleryimg1: "assets/images/g1.jpg",
+        galleryimg2: "assets/images/g2.jpg",
+        galleryimg3: "assets/images/g3.jpg",
+        galleryimg4: "assets/images/g4.jpg"),
+    UserData(
+        id: 1,
+        name: "Selina kyle",
+        desc: "Trainer, Nutriotionist, consultant, Physiotherapist etc.",
+        location: "Fuerstenfeldbruck, Germa... ",
+        groupprice: 10,
+        personprice: 15,
+        consultingprice: 30,
+        userimage: "assets/images/user.jpg",
+        galleryimg1: "assets/images/g1.jpg",
+        galleryimg2: "assets/images/g2.jpg",
+        galleryimg3: "assets/images/g3.jpg",
+        galleryimg4: "assets/images/g4.jpg"),
+    UserData(
+        id: 1,
+        name: "Selina kyle",
+        desc: "Trainer, Nutriotionist, consultant, Physiotherapist etc.",
+        location: "Fuerstenfeldbruck, Germa... ",
+        groupprice: 10,
+        personprice: 15,
+        consultingprice: 30,
+        userimage: "assets/images/user.jpg",
+        galleryimg1: "assets/images/g1.jpg",
+        galleryimg2: "assets/images/g2.jpg",
+        galleryimg3: "assets/images/g3.jpg",
+        galleryimg4: "assets/images/g4.jpg"),
+    UserData(
+        id: 1,
+        name: "Selina kyle",
+        desc: "Trainer, Nutriotionist, consultant, Physiotherapist etc.",
+        location: "Fuerstenfeldbruck, Germa... ",
+        groupprice: 10,
+        personprice: 15,
+        consultingprice: 30,
+        userimage: "assets/images/user.jpg",
+        galleryimg1: "assets/images/g1.jpg",
+        galleryimg2: "assets/images/g2.jpg",
+        galleryimg3: "assets/images/g3.jpg",
+        galleryimg4: "assets/images/g4.jpg"),
+    UserData(
+        id: 1,
+        name: "Selina kyle",
+        desc: "Trainer, Nutriotionist, consultant, Physiotherapist etc.",
+        location: "Fuerstenfeldbruck, Germa... ",
+        groupprice: 10,
+        personprice: 15,
+        consultingprice: 30,
+        userimage: "assets/images/user.jpg",
+        galleryimg1: "assets/images/g1.jpg",
+        galleryimg2: "assets/images/g2.jpg",
+        galleryimg3: "assets/images/g3.jpg",
+        galleryimg4: "assets/images/g4.jpg"),
+    UserData(
+        id: 1,
+        name: "Selina kyle",
+        desc: "Trainer, Nutriotionist, consultant, Physiotherapist etc.",
+        location: "Fuerstenfeldbruck, Germa... ",
+        groupprice: 10,
+        personprice: 15,
+        consultingprice: 30,
+        userimage: "assets/images/user.jpg",
+        galleryimg1: "assets/images/g1.jpg",
+        galleryimg2: "assets/images/g2.jpg",
+        galleryimg3: "assets/images/g3.jpg",
+        galleryimg4: "assets/images/g4.jpg"),
+    UserData(
+        id: 1,
+        name: "Selina kyle",
+        desc: "Trainer, Nutriotionist, consultant, Physiotherapist etc.",
+        location: "Fuerstenfeldbruck, Germa... ",
+        groupprice: 10,
+        personprice: 15,
+        consultingprice: 30,
+        userimage: "assets/images/user.jpg",
+        galleryimg1: "assets/images/g1.jpg",
+        galleryimg2: "assets/images/g2.jpg",
+        galleryimg3: "assets/images/g3.jpg",
+        galleryimg4: "assets/images/g4.jpg"),
+    UserData(
+        id: 1,
+        name: "Selina kyle",
+        desc: "Trainer, Nutriotionist, consultant, Physiotherapist etc.",
+        location: "Fuerstenfeldbruck, Germa... ",
+        groupprice: 10,
+        personprice: 15,
+        consultingprice: 30,
+        userimage: "assets/images/user.jpg",
+        galleryimg1: "assets/images/g1.jpg",
+        galleryimg2: "assets/images/g2.jpg",
+        galleryimg3: "assets/images/g3.jpg",
+        galleryimg4: "assets/images/g4.jpg")
+  ];
+}
